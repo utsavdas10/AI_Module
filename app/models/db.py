12fb6eb2a008742167d4b20a3307b6fae442258b
utsavdas10 <97392403+utsavdas10@users.noreply.github.com>
2025-08-04 13:10:42 +0530

    model changes

--- app/models/db.py
+++ app/models/db.py
@@ -6,11 +6,14 @@ from typing import Literal, Optional
 class DBConnectionParams(BaseModel):
     id: str = Field(..., description="A unique identifier for this database connection, e.g., 'postgres_prod' or 'mongo_logs'.")
     db_type: Literal["postgresql", "mysql", "mongodb"]
-    host: str # For Mongo Atlas, this will be the cluster URI (e.g., "cluster0.abcde.mongodb.net")
-    port: Optional[int] = None
-    username: str
-    password: SecretStr
-    database: str
     ssl_mode: Optional[str] = "prefer"
+    connection_string: Optional[str] = None
+
+    host: Optional[str] = None  # For Mongo Atlas, this will be the cluster URI (e.g., "cluster0.abcde.mongodb.net")
+    port: Optional[int] = None
+    username: Optional[str] = None
+    password: Optional[SecretStr] = None
+    database: Optional[str] = None
+    
 
     model_config = ConfigDict(extra="forbid")
